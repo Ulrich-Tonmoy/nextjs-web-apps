767072f0d3c147267afdc80a0872b5fd75e6d8c8
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2022-03-12 10:17:10 +0600

    Home Page Done

--- realstate/pages/_app.js
+++ realstate/pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import Router from "next/router";
+import Head from "next/head";
+import NProgress from "nprogress";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head></Head>
+            <ChakraProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ChakraProvider>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
--- realstate/pages/index.js
+++ realstate/pages/index.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+import Property from "../components/Property";
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
     return (
         <Box>
             <Banner
@@ -15,7 +17,11 @@ export default function Home() {
                 linkName="/search?purpose=for-rent"
                 imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
             />
-            <Flex flexWrap="wrap">{/* {TODO:} */}</Flex>
+            <Flex flexWrap="wrap">
+                {propertiesForRent.map((property) => (
+                    <Property property={property} key={property.id} />
+                ))}
+            </Flex>
             <Banner
                 purpose="Buy A Home"
                 title1="Find, Buy & own Your"
@@ -26,6 +32,11 @@ export default function Home() {
                 linkName="/search?purpose=for-sale"
                 imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
             />
+            <Flex flexWrap="wrap">
+                {propertiesForSale.map((property) => (
+                    <Property property={property} key={property.id} />
+                ))}
+            </Flex>
         </Box>
     );
 }
@@ -50,3 +61,19 @@ const Banner = ({ purpose, imageUrl, title1, title2, desc1, desc2, linkName, but
         </Box>
     </Flex>
 );
+
+export async function getStaticProps() {
+    const propertiesForSale = await fetchApi(
+        `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
+    );
+    const propertiesForRent = await fetchApi(
+        `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
+    );
+
+    return {
+        props: {
+            propertiesForSale: propertiesForSale?.hits,
+            propertiesForRent: propertiesForRent?.hits,
+        },
+    };
+}
