f830e673f79f1edef84ed25e331ffa57786a655a
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2022-03-15 10:48:55 +0600

    search by location added

--- realstate/components/SearchFilters.jsx
+++ realstate/components/SearchFilters.jsx
@@ -5,10 +5,15 @@ import { MdCancel } from "react-icons/md";
 import Image from "next/image";
 import { filterData, getFilterValues } from "../utils/filterData";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
+import noresult from "../assets/noresult.svg";
 
 export default function SearchFilters() {
     const [filters, setFilters] = useState(filterData);
     const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState("");
+    const [locationData, setLocationData] = useState();
+    const [showLocations, setShowLocations] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const searchProperties = (filterValues) => {
         const path = router.pathname;
@@ -24,6 +29,19 @@ export default function SearchFilters() {
 
         router.push({ pathname: path, query });
     };
+
+    useEffect(() => {
+        if (searchTerm !== "") {
+            const fetchData = async () => {
+                setLoading(true);
+                const data = await fetchApi(`${baseUrl}/auto-complete?query=${searchTerm}`);
+                setLoading(false);
+                setLocationData(data?.hits);
+            };
+
+            fetchData();
+        }
+    }, [searchTerm]);
 
     return (
         <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
@@ -43,6 +61,79 @@ export default function SearchFilters() {
                     </Select>
                 </Box>
             ))}
+            <Flex flexDir="column">
+                <Button
+                    onClick={() => setShowLocations(!showLocations)}
+                    border="1px"
+                    borderColor="gray.200"
+                    marginTop="2"
+                >
+                    Search Location
+                </Button>
+                {showLocations && (
+                    <Flex flexDir="column" pos="relative" paddingTop="2">
+                        <Input
+                            placeholder="Type Here"
+                            value={searchTerm}
+                            w="300px"
+                            focusBorderColor="gray.300"
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        {searchTerm !== "" && (
+                            <Icon
+                                as={MdCancel}
+                                pos="absolute"
+                                cursor="pointer"
+                                right="5"
+                                top="5"
+                                zIndex="100"
+                                onClick={() => setSearchTerm("")}
+                            />
+                        )}
+                        {loading && <Spinner margin="auto" marginTop="3" />}
+                        {showLocations && (
+                            <Box height="300px" overflow="auto">
+                                {locationData?.map((location) => (
+                                    <Box
+                                        key={location.id}
+                                        onClick={() => {
+                                            searchProperties({
+                                                locationExternalIDs: location.externalID,
+                                            });
+                                            setShowLocations(false);
+                                            setSearchTerm(location.name);
+                                        }}
+                                    >
+                                        <Text
+                                            cursor="pointer"
+                                            bg="gray.200"
+                                            p="2"
+                                            borderBottom="1px"
+                                            borderColor="gray.100"
+                                        >
+                                            {location.name}
+                                        </Text>
+                                    </Box>
+                                ))}
+                                {!loading && !locationData?.length && (
+                                    <Flex
+                                        justifyContent="center"
+                                        alignItems="center"
+                                        flexDir="column"
+                                        marginTop="5"
+                                        marginBottom="5"
+                                    >
+                                        <Image src={noresult} alt="no result" />
+                                        <Text fontSize="xl" marginTop="3">
+                                            Waiting to search!
+                                        </Text>
+                                    </Flex>
+                                )}
+                            </Box>
+                        )}
+                    </Flex>
+                )}
+            </Flex>
         </Flex>
     );
 }
