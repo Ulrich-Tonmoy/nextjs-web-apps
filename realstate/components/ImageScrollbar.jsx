eb39f295b2113412972f1c4686f6aaf09e466fd8
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2022-03-15 10:33:25 +0600

    property details page added

---
+++ realstate/components/ImageScrollbar.jsx
@@ -0,0 +1,57 @@
+import { useContext } from "react";
+import Image from "next/image";
+import { Box, Icon, Flex } from "@chakra-ui/react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+
+export default function ImageScrollbar({ images }) {
+    return (
+        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: "hidden" }}>
+            {images.map((image) => (
+                <Box width="910px" itemId={image.id} overflow="hidden" p="1" key={image.id}>
+                    <Image
+                        placeholder="blur"
+                        blurDataURL={image.url}
+                        src={image.url}
+                        width={1000}
+                        height={500}
+                        alt="property image"
+                        sizes="(max-width:500px) 100px, (max-height:1024px) 400px, 1000px"
+                    />
+                </Box>
+            ))}
+        </ScrollMenu>
+    );
+}
+
+const LeftArrow = () => {
+    const { scrollPrev } = useContext(VisibilityContext);
+
+    return (
+        <Flex justifyContent="center" alignItems="center" marginRight="1">
+            <Icon
+                as={FaArrowAltCircleLeft}
+                onClick={() => scrollPrev()}
+                fontSize="2xl"
+                cursor="pointer"
+                d={["none", "none", "none", "block"]}
+            />
+        </Flex>
+    );
+};
+
+const RightArrow = () => {
+    const { scrollNext } = useContext(VisibilityContext);
+
+    return (
+        <Flex justifyContent="center" alignItems="center" marginLeft="1">
+            <Icon
+                as={FaArrowAltCircleRight}
+                onClick={() => scrollNext()}
+                fontSize="2xl"
+                cursor="pointer"
+                d={["none", "none", "none", "block"]}
+            />
+        </Flex>
+    );
+};
