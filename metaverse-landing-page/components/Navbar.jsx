d37e239e9b37a5a1d404eb33ac63d79c0e75fc12
Tonmoy <ulrichtonmoy1@gmail.com>
2022-12-04 03:57:38 +0600

    metaverse-landing-page

---
+++ metaverse-landing-page/components/Navbar.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { navVariants } from "../utils/motion";
+import styles from "../styles";
+
+const Navbar = () => (
+    <motion.nav
+        variants={navVariants}
+        initial="hidden"
+        whileInView="show"
+        className={`${styles.xPaddings} py-8 relative`}
+    >
+        <div className="absolute w-[50%] inset-0 gradient-01" />
+        <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+            <img src="/search.svg" alt="search icon" className="w-[24px] h-[24px] object-contain" />
+            <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">METAVERSE</h2>
+            <img src="/menu.svg" alt="menu icon" className="w-[24px] h-[24px] object-contain" />
+        </div>
+    </motion.nav>
+);
+
+export default Navbar;
