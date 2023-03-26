cca2719f0ee7c77637c55af07a472ae901236c1b
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-27 05:50:33 +0600

    🔧 config file update

--- rapid-api/tailwind.config.js
+++ rapid-api/tailwind.config.js
@@ -1,5 +1,9 @@
+const { colors } = require("tailwindcss/colors");
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -9,7 +13,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.5rem",
+        screens: {
+          "2xl": "1360px",
+        },
+      },
+      extend: {
+        fontFamily: {
+          sans: ["var(--font-inter)", ...fontFamily.sans],
+        },
+        colors: {
+          ...colors,
+          "light-gold": "#f5bc51",
+          "dark-gold": "#533519",
+        },
+      },
+    },
   },
-  plugins: [],
+  // plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
