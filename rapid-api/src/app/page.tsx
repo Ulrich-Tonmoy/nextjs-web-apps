ef4567701ec1296840f375cd92b6105b21d59986
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-26 06:27:54 +0600

    🎉 the beginning

---
+++ rapid-api/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <main className="bg-red-500">Hello World</main>;
+}
