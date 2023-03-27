a9ff6aa075edd34c849dc63e9c4d054abc78850d
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-27 07:38:13 +0600

    💄 theme toggle icons

--- rapid-api/src/components/ThemeToggle.tsx
+++ rapid-api/src/components/ThemeToggle.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuTrigger,
 } from "@/ui/DropdownMenu";
 import Button from "@/ui/Button";
-import { Laptop, Moon, Sun } from "lucide-react";
+import Icons from "@/components/Icons";
 
 interface ThemeToggleProps {}
 
@@ -20,22 +20,22 @@ const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm">
-          <Sun className="transition-all scale-100 rotate-0 hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
-          <Moon className="absolute transition-all scale-0 rotate-90 hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
+          <Icons.Sun className="transition-all scale-100 rotate-0 hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
+          <Icons.Moon className="absolute transition-all scale-0 rotate-90 hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" forceMount>
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          <Sun className="w-4 h-4 mr-2" />
+          <Icons.Sun className="w-4 h-4 mr-2" />
           <span>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <Moon className="w-4 h-4 mr-2" />
+          <Icons.Moon className="w-4 h-4 mr-2" />
           <span>Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          <Laptop className="w-4 h-4 mr-2" />
+          <Icons.Laptop className="w-4 h-4 mr-2" />
           <span>System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
