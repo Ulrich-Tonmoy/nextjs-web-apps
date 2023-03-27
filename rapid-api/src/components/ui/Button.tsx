6a7a00d53b01fc6ca3d028d058f3676b8e3413b1
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-27 06:59:18 +0600

    💄 update buttonvaritant export

--- rapid-api/src/components/ui/Button.tsx
+++ rapid-api/src/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import { VariantProps, cva } from "class-variance-authority";
 import { Loader2 } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900",
   {
     variants: {
