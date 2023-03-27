594344552e9f1b4a7ce50867455826fa4940dcfc
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-27 06:25:36 +0600

    🚚 folder restructured

--- rapid-api/src/components/ui/LargeHeading.tsx
+++ rapid-api/src/components/ui/LargeHeading.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import { cn } from "../../lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
 const headingVariants = cva(
   "text-black dark:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter",
