cbd5b4224d77e7a521a5e95ce7342cbe28d7da8e
Tonmoy <ulrichtonmoy1@gmail.com>
2023-04-01 05:01:31 +0600

    ⚰️

--- rapid-api/src/app/(docs)/documentation/layout.tsx
+++
@@ -1,6 +0,0 @@
-import "@/styles/globals.css";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  // offset navbar height
-  return <section className="pt-20">{children}</section>;
-}
