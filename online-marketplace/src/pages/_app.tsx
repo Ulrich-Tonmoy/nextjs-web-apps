66a3a4077ce59fd248a48ad0ba947024305c22c4
Tonmoy <ulrichtonmoy1@gmail.com>
2023-04-21 04:41:52 +0600

    🔨 added navbar and auth functionality

--- online-marketplace/src/pages/_app.tsx
+++ online-marketplace/src/pages/_app.tsx
@@ -1,11 +1,31 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+import { dark } from "@clerk/themes";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { NavBar } from "~/components/Navbar";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        layout: {
+          socialButtonsVariant: "iconButton",
+          socialButtonsPlacement: "bottom",
+        },
+      }}
+      {...pageProps}
+    >
+      <div className="light:black bg-gray-800 dark:text-white">
+        <NavBar />
+        <Component {...pageProps} />
+      </div>
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
