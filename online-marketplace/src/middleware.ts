cec9d1fb7b3c5e5e08bdd0d60c47a803663d475b
Tonmoy <ulrichtonmoy1@gmail.com>
2023-04-22 07:43:46 +0600

    :hammer: add item to sell

--- online-marketplace/src/middleware.ts
+++ online-marketplace/src/middleware.ts
@@ -20,7 +20,7 @@ export default withClerkMiddleware((request: NextRequest) => {
 
   if (!userId) {
     // redirect the users to /pages/sign-in/[[...index]].ts
-    const signInUrl = new URL("/sign-in", request.url);
+    const signInUrl = new URL("/", request.url);
     signInUrl.searchParams.set("redirect_url", request.url);
     return NextResponse.redirect(signInUrl);
   }
