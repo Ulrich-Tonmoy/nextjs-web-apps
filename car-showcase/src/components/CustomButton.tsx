90a1cb46d57723e179b44e7b9e4c2b581cd0a294
Tonmoy <ulrichtonmoy1@gmail.com>
2023-06-19 00:17:21 +0600

    chore: custom button comp added

---
+++ car-showcase/src/components/CustomButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+const CustomButton = ({
+  isDisabled,
+  btnType,
+  containerStyles,
+  textStyles,
+  title,
+  rightIcon,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={isDisabled}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} alt="arrow_left" fill className="object-contain" />
+        </div>
+      )}
+    </button>
+  );
+};
+
+export default CustomButton;
