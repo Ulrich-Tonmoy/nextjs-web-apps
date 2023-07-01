ad018a6e6f42c05dd390d9bf5f322a386b6fdc64
Tonmoy <ulrichtonmoy1@gmail.com>
2023-07-01 19:42:38 +0600

    🤡

--- car-showcase/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import Hero from "./Hero";
-import CustomButton from "./CustomButton";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import SearchBar from "./SearchBar";
-import CustomFilter from "./CustomFilter";
-import SearchManufacturer from "./SearchManufacturer";
-
-export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer };
