ac00d68006249ae0f6e1acca32cfa841c4978965
Ulrich <ulrichtonmoy1@gmail.com>
2023-08-07 19:06:16 +0600

    chore: onboarding

---
+++ threads/src/lib/validations/user.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const UserValidation = z.object({
+  profile_picture: z.string().url().nonempty(),
+  name: z.string().min(3).max(30),
+  username: z.string().min(3).max(30),
+  bio: z.string().min(3).max(1000),
+});
