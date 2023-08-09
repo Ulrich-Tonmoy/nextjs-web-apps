3c5554129affaa1140633e8f00d1344c1226bec6
Ulrich <ulrichtonmoy1@gmail.com>
2023-08-09 16:03:15 +0600

    chore: search page

---
+++ threads/src/app/(root)/communities/page.tsx
@@ -0,0 +1,9 @@
+const Page = async () => {
+  return (
+    <section>
+      <h1 className="head-text mb-11">Communities</h1>
+    </section>
+  );
+};
+
+export default Page;
---
+++ threads/src/app/(root)/search/page.tsx
@@ -0,0 +1,45 @@
+import UserCard from "@/components/cards/UserCard";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 20,
+  });
+
+  return (
+    <section>
+      <h1 className="head-text mb-11">Search</h1>
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No Users</p>
+        ) : (
+          <>
+            {result.users.map((user: any, i: number) => (
+              <UserCard
+                key={i}
+                id={user.id}
+                name={user.name}
+                username={user.username}
+                image={user.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Page;
