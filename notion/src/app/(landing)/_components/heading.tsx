24079b58d054895885fb07cfc65df07974946fcc
Tonmoy <ulrichtonmoy1@gmail.com>
2023-10-29 21:37:19 +0600

    chore: landing page

---
+++ notion/src/app/(landing)/_components/heading.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Notion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Notion is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+    </div>
+  );
+};
+
+export default Heading;
