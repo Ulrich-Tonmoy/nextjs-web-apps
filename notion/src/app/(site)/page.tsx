397087072e8ac66d1741522db25a57e81daa8b4f
Tonmoy <ulrichtonmoy1@gmail.com>
2023-11-19 04:42:47 +0600

    fix

--- notion/src/app/(site)/page.tsx
+++ notion/src/app/(site)/page.tsx
@@ -30,15 +30,15 @@ const Home = () => {
             </Button>
           </div>
           <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
-            <Image src={Banner} alt="Application Banner" />
+            <Image src={Banner} alt="Application Banner" priority={true} />
             <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10" />
           </div>
         </div>
       </section>
       <section className="relative">
         <div className="overflow-hidden flex after:content[''] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:top-0 after:w-20 after:z-10 after:absolute before:content[''] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:top-0 before:bottom-0 before:w-20 before:z-10 before:absolute">
-          {[...Array(2)].map((arr) => (
-            <div key={arr} className="flex flex-nowrap animate-slide">
+          {[...Array(2)].map((_, index) => (
+            <div key={index} className="flex flex-nowrap animate-slide">
               {CLIENTS.map((client) => (
                 <div
                   key={client.alt}
@@ -75,7 +75,7 @@ const Home = () => {
             subheading="Join thousands of satisfied users who rely on our platform for their personal and professional productivity needs."
             pill="Testimonials"
           />
-          {[...Array(2)].map((arr, index) => (
+          {[...Array(2)].map((_, index) => (
             <div
               key={index}
               className={twMerge(
